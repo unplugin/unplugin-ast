c2641e0f3fa649269bf8829e8d62bf03fd9f7b56
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-06-09 04:32:03 +0800

    refactor: ESM package

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['./src'],
+  entry: ['./src/*.ts'],
   format: ['cjs', 'esm'],
   target: 'node14',
   splitting: true,
