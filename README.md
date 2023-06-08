7d8d6ef800595e5f09ded26cf05205713c3d98bb
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-06-09 05:05:26 +0800

    refactor!: drop node 14

--- README.md
+++ README.md
@@ -146,4 +146,4 @@ export const RemoveWrapperFunction = (
 
 ## License
 
-[MIT](./LICENSE) License © 2022 [三咲智子](https://github.com/sxzz)
+[MIT](./LICENSE) License © 2022-PRESENT [三咲智子](https://github.com/sxzz)
--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['./src/*.ts'],
   format: ['cjs', 'esm'],
-  target: 'node14',
+  target: 'node16.14',
   splitting: true,
   clean: true,
   dts: true,
