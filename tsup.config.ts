82ec91c3af44d85e9368fb0cb0ac574199ae1628
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-05-03 23:01:31 +0900

    feat!: drop node 16

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['./src/*.ts'],
   format: ['cjs', 'esm'],
-  target: 'node16.14',
+  target: 'node18.12',
   splitting: true,
   cjsInterop: true,
   clean: true,
