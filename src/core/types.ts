92a8a48d79d3bced5bb7cc7112d06c3a2faa1091
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-12-13 22:56:16 +0800

    docs: add jsdoc

--- src/core/types.ts
+++ src/core/types.ts
@@ -13,7 +13,15 @@ export interface NodeRef<T = Node> {
 }
 
 export interface Transformer<T extends Node = Node> {
+  /**
+   * Filter files to transform
+   * @param id - filename
+   * @returns whether to include the file
+   */
   transformInclude?: (id: string) => Awaitable<boolean>
+  /**
+   * Filter nodes to transform
+   */
   onNode?:
     | ((
         node: Node,
@@ -25,6 +33,11 @@ export interface Transformer<T extends Node = Node> {
         parent: Node | null | undefined,
         index: number | null | undefined,
       ) => node is T)
+  /**
+   * Transform the node to a new node or string
+   *
+   * @returns the new node or string, or `false` to remove the node
+   */
   transform: (
     node: T,
     code: string,
@@ -32,5 +45,8 @@ export interface Transformer<T extends Node = Node> {
       id: string
     },
   ) => Awaitable<string | Node | false | undefined | null>
+  /**
+   * It will be called after all nodes are transformed
+   */
   finalize?: (s: MagicStringAST) => Awaitable<void>
 }
